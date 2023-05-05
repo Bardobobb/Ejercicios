7b80902df060cb4b8f5bff4d609ac5493c9be8fe
Bardobobb <132633852+Bardobobb@users.noreply.github.com>
2023-05-04 21:18:57 -0600

    Add files via upload

---
+++ Ejercicio1Poo/Ejercicio1Poo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando un nuevo objeto de la calse vehiculo
+            Vehiculo vehiculo = new Vehiculo(1, "Chevrolet", "Aveo", 5000, 15000, 2017);
+            Console.WriteLine(vehiculo.ToString());
+            Console.ReadKey();
+        }
+    }
+}
