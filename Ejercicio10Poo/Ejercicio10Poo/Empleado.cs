f12dbcbbaa887f7c9a1882800fdff353644aafa6
Bardobobb <132633852+Bardobobb@users.noreply.github.com>
2023-05-04 23:43:47 -0600

    Add files via upload

---
+++ Ejercicio10Poo/Ejercicio10Poo/Empleado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10Poo
+{
+    public class Empleado : Persona
+    {
+        public Empleado(string nombre, int edad, decimal sueldoBruto) : base(nombre, edad)
+        {
+            SueldoBruto = sueldoBruto;
+        }
+
+        public decimal SueldoBruto { get; set; }
+
+        public new void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine($"Sueldo Bruto: {SueldoBruto}");
+        }
+
+        public decimal CalcularSalarioNeto()
+        {
+
+            decimal seguro = SueldoBruto * 0.12m;
+            decimal salarioneto = SueldoBruto - seguro;
+            return salarioneto;
+        }
+    }
+}
---
+++ Ejercicio10Poo/Ejercicio10Poo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona per = new Persona("Maria", 29);
+            per.Mostrar();
+
+            Cliente cli = new Cliente("MArlon", 35, "1234556");
+            cli.Mostrar();
+
+            Empleado emp = new Empleado("Marez", 26, 80000);
+            emp.Mostrar();
+
+            decimal salan = emp.CalcularSalarioNeto();
+            Console.WriteLine("Salario neto: {0}", salan);
+
+            Empresa empresa = new Empresa("El Verdugo");
+
+            Directivo direc = new Directivo("Robert", 49, "Gerente");
+            direc.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
