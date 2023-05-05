91193bada95c1a88c23ffda747a3dbe697558290
Bardobobb <132633852+Bardobobb@users.noreply.github.com>
2023-05-04 23:40:57 -0600

    Add files via upload

---
+++ Ejercicio5Poo/Ejercicio5Poo/Foto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Poo
+{
+    internal class foto
+    {
+        int id;
+        int tamanio;
+        string comentario;
+        DateTime fecha;
+
+        public foto(int id, int tamanio, string comentario, DateTime fecha)
+        {
+            this.Id = id;
+            this.Tamanio = tamanio;
+            this.Comentario = comentario;
+            this.Fecha = fecha;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Tamanio { get => tamanio; set => tamanio = value; }
+        public string Comentario { get => comentario; set => comentario = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+
+        public override string ToString()
+        {
+            return $"id: {Id}\n Tamanio: {Tamanio}\nComentario: {Comentario}nFecha: {Fecha}";
+        }
+    }
+}
---
+++ Ejercicio5Poo/Ejercicio5Poo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            album alb = new album(1, "recuerdos", new DateTime(2015, 10, 20), new List<foto>());
+            foto f1 = new foto(1, 20, "aqui en miami", new DateTime(2013, 12, 24));
+            alb.añadirFoto(f1);
+            alb.mostrarFoto();
+            alb.tamañoAlbum();
+            alb.vaciarAlbum();
+            alb.eliminarFoto(f1);
+            Console.ReadKey();
+        }
+    }
+}
