ba000d364cee4afb6aa2a6fce96afafbfa95c860
Bardobobb <132633852+Bardobobb@users.noreply.github.com>
2023-05-04 21:20:08 -0600

    Add files via upload

---
+++ Ejercicio2Poo/Ejercicio2Poo/CasaComercial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Poo
+{
+    class CasaComercial
+    {
+        //Campos de la clase casa comercial
+        string nombre;
+        string email;
+        int total;
+        List<Vehiculo> vehiculos;
+
+        //Constructor
+
+        public CasaComercial(string nombre, string email, int total, List<Vehiculo> vehiculos)
+        {
+            this.Nombre = nombre;
+            this.Email = email;
+            this.Total = total;
+            this.Vehiculos = vehiculos;
+        }
+
+        //Propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Email { get => email; set => email = value; }
+        public int Total { get => total; set => total = value; }
+        internal List<Vehiculo> Vehiculos { get => vehiculos; set => vehiculos = value; }
+
+        //Añadimos cada uno de los metodos
+        public void AniadirVehiculo(Vehiculo vehiculo)
+        {
+            try
+            {
+                //añadimos un nuevo vehiculo a lista vehiculo
+                this.vehiculos.Add(vehiculo);
+                //Actualizamos el total de vehiculos
+                this.total += 1;
+                Console.WriteLine("Se ha añadido un nuevo vehiculo\n");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se pudo añadir debido a: {0}", ex.Message);
+                Console.ReadLine();
+            }
+        }
+        public void Mostrarvehiculos()
+        {
+            foreach (var n in this.vehiculos)
+            {
+                Console.WriteLine("----------");
+                n.ToString();
+            }
+        }
+        public void VaciarVehiculo()
+        {
+            Console.WriteLine("Estas seguro que desea eliminar todos los elementos mostrados en la lista: S/N");
+            string resp = Console.ReadLine();
+            if (resp == "S" || resp == "s")
+            {
+                vehiculos = new List<Vehiculo>();
+                this.total = 0;
+                Console.WriteLine("Se ha eliminado todos los elementos");
+            }
+            else
+            {
+                Console.WriteLine("No se elimino, Hay {0} vehiculo registrado", total);
+                Console.ReadKey();
+            }
+        }
+        public void EliminarVehiculo(Vehiculo v)
+        {
+            vehiculos.Remove(v);
+            total -= 1;
+            Console.WriteLine("Se ha eliminado un registro");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio2Poo/Ejercicio2Poo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("De el nombre de la casa comercial: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingresa el email: ");
+            string email = Console.ReadLine();
+
+            CasaComercial cc = new CasaComercial(nombre, email, 0, new List<Vehiculo>());
+            Console.WriteLine("Informacion del vehiculo a añadir");
+            Console.WriteLine("Modelo: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Marca: ");
+            string marca = Console.ReadLine();
+            Console.WriteLine("kilometraje: ");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Año: ");
+            int annio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Precio: ");
+            decimal precio = decimal.Parse(Console.ReadLine());
+            //info del nuevo vehiculo usando añadir vehiculo
+            cc.AniadirVehiculo(new Vehiculo(1, marca, model, km, precio, annio));
+            Console.WriteLine("\nAcontinuacion mostrara todos los vehiculos de la lista: ");
+            cc.Mostrarvehiculos();
+            Console.WriteLine("Desea vaciar la lista: S/N");
+            string rep = Console.ReadLine();
+            if (rep == "s" || rep == "S")
+            {
+                cc.VaciarVehiculo();
+            }
+            Console.ReadKey();
+        }
+    }
+}
