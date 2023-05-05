966505f1594aba1aebc7ed9a1a85dd08da6e023c
Bardobobb <132633852+Bardobobb@users.noreply.github.com>
2023-05-04 23:42:50 -0600

    Add files via upload

---
+++ Ejercicio8Poo/Ejercicio8Poo/Pelicula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8Poo
+{
+    class Pelicula
+    {
+        string nombre;
+        int anio;
+        private List<Actor> actores;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Anio { get => anio; set => anio = value; }
+        internal List<Actor> Actores { get => actores; set => actores = value; }
+
+        public Pelicula(string nombre, int anio, List<Actor> actores)
+        {
+            this.Nombre = nombre;
+            this.Anio = anio;
+            this.Actores = actores;
+        }
+        public void AgregarActor(Actor actor)
+        {
+            Actores.Add(actor);
+        }
+    }
+}
---
+++ Ejercicio8Poo/Ejercicio8Poo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudio estudio = new Estudio("holiwood", "Calle bla", new List<Pelicula>());
+            Pelicula pelicula = new Pelicula("La Muerte", 2000, new List<Actor>());
+            Actor actor = new Actor("Leonardo", "Di Caprio");
+            Console.WriteLine("Nombre del estudio: {0}\nDireccion: {1}\nNombre de la pelicula: {2}\nAño de la pelicula: {3}\nNombre del actor: {4}\nApellido del actor: {5}.", estudio.Nombre, estudio.Direccion, pelicula.Nombre, pelicula.Anio, actor.Nombre, actor.Apellido1);
+            Console.ReadKey();
+        }
+    }
+}
